6b1405f34cdaa891737a6f4fe302d099f3154917
Patrick <patrick_gadd@hotmail.com>
2014-10-09 18:35:49 +0200

    Alley a la page 170

--- src/Alley.java
+++ src/Alley.java
@@ -9,30 +9,79 @@ public class Alley {
     Direction dir;
     int count;
 
+    int upCount, downCount;
+    Semaphore alleyFree, mutexUp, mutexDown;
+
     public Alley(CarDisplayI cd) {
         this.cd = cd;
         this.sem = new Semaphore(1);
         this.count = 0;
+
+
+        this.upCount = 0;
+        this.downCount = 0;
+        this.alleyFree = new Semaphore(1);
+        this.mutexDown = new Semaphore(1);
+        this.mutexUp = new Semaphore(1);
     }
 
     public void enter(int no) throws InterruptedException {
         Direction newDir = (no < 5) ? Direction.UP : Direction.DOWN;
-        this.sem.P();
 
-        if (this.isEmpty() || newDir == this.dir) {
-            cd.println("Entering no: " + no);
-            this.dir = newDir;
-            this.count++;
+        if (newDir == Direction.UP) {
+            this.mutexUp.P();
+
+            this.upCount++;
+            if (this.upCount == 1)
+                this.alleyFree.P();
+
+            this.mutexUp.V();
+        }else {
+            this.mutexDown.P();
+
+            this.downCount++;
+            if (this.downCount == 1)
+                this.alleyFree.P();
+
+            this.mutexDown.P();
+//            this.alleyFree.P();
         }
-        else {
-            cd.println("Could not enter no: " + no + " dir: " + this.dir.toString());
-        }
+//        this.sem.P();
+//
+//
+//
+//        if (this.isEmpty() || newDir == this.dir) {
+//            cd.println("Entering no: " + no);
+//            this.dir = newDir;
+//            this.count++;
+//        }
+//        else {
+//            cd.println("Could not enter no: " + no + " dir: " + this.dir.toString());
+//        }
     }
 
-    public void leave(int no){
+    public void leave(int no) throws InterruptedException{
         cd.println("Leaving no: " + no);
-        this.sem.V();
-        this.count--;
+        Direction newDir = (no < 5) ? Direction.UP : Direction.DOWN;
+
+        if (newDir == Direction.UP) {
+            this.mutexUp.P();
+
+            this.upCount--;
+            if(this.upCount == 0)
+                this.alleyFree.V();
+        }else {
+            this.mutexDown.P();
+
+            this.downCount--;
+            if(this.downCount == 0)
+                this.alleyFree.V();
+//            this.alleyFree.V();
+        }
+
+
+//        this.sem.V();
+//        this.count--;
     }
 
     private boolean isEmpty() {
