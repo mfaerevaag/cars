95365c1843657fb564bd05213563d363ed267ef8
Patrick <patrick_gadd@hotmail.com>
2014-11-09 18:02:37 +0100

    And now the barrier also works when turned on/off

--- src/Barrier.java
+++ src/Barrier.java
@@ -1,3 +1,7 @@
+enum BarrierSelector {
+    INCOMING, LEAVING
+}
+
 public class Barrier {
 
     private boolean active;
@@ -37,7 +41,7 @@ public class Barrier {
                 this.barrierIncoming.P();
 
             } else { // Final car needed to start a new round
-                this.freeIncoming(this.incomingCount - 1);
+                this.free(this.incomingCount - 1, BarrierSelector.INCOMING);
                 this.incomingCount = 0;
                 this.mutex.V();
             }
@@ -56,7 +60,7 @@ public class Barrier {
                 this.barrierLeaving.P();
 
             }else {
-                this.freeLeaving(this.leavingCount - 1);
+                this.free(this.leavingCount - 1, BarrierSelector.LEAVING);
                 this.leavingCount = 0;
                 this.debug_carsRounds[carNo]++;//TODO: remove
                 this.checkInvariant(carNo);//TODO: remove
@@ -96,6 +100,9 @@ public class Barrier {
             if (!this.active) {
                 this.active = true;
 
+                this.incomingCount = 0;
+                this.leavingCount = 0;
+
                 for (int i = 0; i < this.debug_carsRounds.length; i++) {
                     this.debug_carsRounds[i] = 0;
                 }
@@ -123,8 +130,8 @@ public class Barrier {
                 }
 
                 this.active = false;
-                this.freeIncoming(this.incomingCount);
-                this.freeLeaving(this.incomingCount);
+                this.free(this.incomingCount, BarrierSelector.INCOMING);
+                this.free(this.leavingCount+this.incomingCount, BarrierSelector.LEAVING); //leavingCount PLUS incomingCount, as the just released incomings will need to pass the leaving barrier
                 this.incomingCount = 0;
                 this.leavingCount = 0;
             }
@@ -135,189 +142,27 @@ public class Barrier {
         }
     }
 
-    private void freeIncoming(int n) {
-        // last car does not P(), so no need to release it
-        for (int i = 0; i < n; i++)
-            this.barrierIncoming.V();
+    private void free(int n, BarrierSelector selector) {
+        for (int i = 0; i < n; i++) {
+            if (selector == BarrierSelector.INCOMING)
+                this.barrierIncoming.V();
+            else
+                this.barrierLeaving.V();
+        }
+
     }
 
-    private void freeLeaving(int n) {
-        // last car does not P(), so no need to release it
-        for (int i = 0; i < n; i++)
-            this.barrierLeaving.V();
-    }
+//    private void freeIncoming(int n) {
+//        // last car does not P(), so no need to release it
+//        for (int i = 0; i < n; i++)
+//            this.barrierIncoming.V();
+//    }
+//
+//    private void freeLeaving(int n) {
+//        // last car does not P(), so no need to release it
+//        for (int i = 0; i < n; i++)
+//            this.barrierLeaving.V();
+//    }
 
 
 }
-
-//import javax.sound.midi.SysexMessage;
-//import java.util.Arrays;
-//
-//public class Barrier {
-//
-//    private boolean active;
-//    private int threshold;
-//    private int count;
-//    private int roundCount;
-//
-//    // Dealing with speeding
-//    private int lowestRoundCount; //Save the lowest carCurrentRound. If any car is
-//    private int speederCounts;
-//
-//    private int[] carCurrentRound;
-//    private Semaphore barrier, mutex;
-//    private CarDisplayI cd;
-//
-//    public Barrier(CarDisplayI cd) {
-//        this.cd = cd;
-//        this.active = false;
-//        this.threshold = 9; // TODO: extend
-//        this.count = 0;
-//        this.roundCount = 0;
-//
-//        this.lowestRoundCount = 0;
-//        this.speederCounts = 0;
-//
-//        this.carCurrentRound = new int[9];
-//        this.barrier = new Semaphore(0);
-//        this.mutex = new Semaphore(1);
-//    }
-//
-//    /**
-//     * Wait for others to arrive (if barrier active)
-//     */
-//    public void sync(int carNo) throws Exception {
-//
-//        this.mutex.P();
-//
-//        this.invariantCheck(carNo);
-////        System.out.println("Car # " + carNo + " calling .sync()");
-//
-//        boolean activeCopy = this.active;
-////        this.mutex.V();
-//
-//        if (activeCopy) {
-////            this.mutex.P();
-//
-//            this.count++;
-////            System.out.println("Car # " + carNo + " was the car no. " + this.count + " to arrive this round.");
-//            if (this.count < this.threshold) { // All cars, except one
-//
-//                boolean freeToIgnore = this.carCurrentRound[carNo] < this.roundCount;
-//                System.out.println("Car #" + carNo + " carRound: " + this.carCurrentRound[carNo] + ", barrierRound: " + this.roundCount);
-//                this.mutex.V();
-//
-//                if (! freeToIgnore)
-//                    this.barrier.P();
-//
-//                this.mutex.P();
-//                this.carCurrentRound[carNo]++;
-////                System.out.println("Car # "+ carNo + "left: " + Arrays.toString(this.carCurrentRound));
-//                this.mutex.V();
-//
-//            } else { // Final car needed to start a new round
-//
-//                this.roundCount++;
-//                this.free(this.count - 1);
-//
-//                this.count = 0;
-////                System.out.println("Car # " + carNo + " was the last to arrive this round.");
-////                System.out.println(Arrays.toString(this.carCurrentRound));
-//                this.carCurrentRound[carNo]++;
-//                this.mutex.V();
-//            }
-//        }else {
-//            this.mutex.V();
-//        }
-//
-//        this.mutex.P();
-//
-//        this.invariantCheck(carNo);
-//
-//        this.mutex.V();
-//
-//    }
-//
-//    /**
-//     * Activate barrier
-//     */
-//    public void on() {
-//
-//        try {
-//
-//            this.mutex.P();
-//            if (!this.active) {
-//                this.active = true;
-//
-//                this.roundCount = 0;
-//                this.carCurrentRound = new int[9];
-//            }
-//            this.mutex.V();
-//
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    /**
-//     * Deactivate barrier
-//     */
-//    public void off() {
-//
-//        try {
-//            this.mutex.P();
-//            if (this.active) {
-//
-//                this.active = false;
-//                this.free(this.count);
-//                this.count = 0;
-//
-//            }
-//            this.mutex.V();
-//
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    private void free(int n) {
-//        // last car does not P(), so no need to release it
-//        for (int i = 0; i < n; i++)
-//            this.barrier.V();
-//    }
-//
-//    private int getLowestRoundCount() {
-//        int min = 100000000;
-//        for(int i = 0; i < this.carCurrentRound.length; i++) {
-//            if (this.carCurrentRound[i] < min)
-//                min = this.carCurrentRound[i];
-//        }
-//
-//        return min;
-//
-//    }
-//
-//    private void invariantCheck(int carNo) throws Exception{
-//        //Find min and max
-//        int min = 100000000;
-//        int max = -1;
-//
-//        for(int i = 0; i < this.carCurrentRound.length; i++) {
-//            if (this.carCurrentRound[i] < min)
-//                min = this.carCurrentRound[i];
-//
-//            if (this.carCurrentRound[i] > max)
-//                max = this.carCurrentRound[i];
-//
-//        }
-//
-//        if (max - min > 1) {
-//            System.out.println("Car #" + carNo + " carRound: " + this.carCurrentRound[carNo] + ", barrierRound: " + this.roundCount);
-//            System.out.println("Lowest roundCount: " + this.getLowestRoundCount());
-//            this.cd.println("Error, invariant violated!");
-//            throw (new Exception("Error, invariant violated!"));
-//        }
-//
-//
-//    }
-//}
